a82cfb3ff19caff39c1ae6300c1fc16a0fb47651
bront <bront.davis@uky.edu>
2024-04-01 16:50:07 -0400

    Update main.py
    111

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-print ('A beginning is a very delicate time...')
-
 def testing():
-    print ('A beginning is a very delicate time...')
+    print ('A beginning is a very delicate time... 111 ')
   
 if __name__ == '__main__':
     testing()
